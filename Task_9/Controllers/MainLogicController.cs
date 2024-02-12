85efe246b4bd7896b57b9628389b6f7463e31f02
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-12 21:52:06 +0500

    Add limit for concurrent requestinos in main program

--- Task_9/Controllers/MainLogicController.cs
+++ Task_9/Controllers/MainLogicController.cs
@@ -11,116 +11,134 @@ namespace Task_9.Controllers
     [ApiController]
     public class MainLogicController : ControllerBase
     {
+        private static readonly IConfiguration jsonConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(jsonConfig.GetSection("Settings:ParallelLimit").Get<int>());
+
         [HttpGet]
         public ActionResult Get(string inputLine, int sortOption)
         {
-            string badRequestLine;
-            IConfiguration jsonConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            ResultData resultData = new ResultData();
-
-            if (!Regex.IsMatch(inputLine, "^[a-z]+$"))
+            if (!semaphore.Wait(0))
             {
-                if (Regex.Replace(inputLine, " ", "") != "")
+                return BadRequest("Слишком много запросов");
+            }
+
+            try
+            {
+                string badRequestLine;
+
+                ResultData resultData = new ResultData();
+
+                //Checking concurrent requests
+                //Thread.Sleep(5000);
+
+                if (!Regex.IsMatch(inputLine, "^[a-z]+$"))
                 {
-                    badRequestLine = "В строке должны быть только латинские буквы в нижнем регистре! Неподходящие символы: " + 
-                        Regex.Replace(Regex.Replace(inputLine, "[a-z]", ""), " ", " 'Пробел' ");
+                    if (Regex.Replace(inputLine, " ", "") != "")
+                    {
+                        badRequestLine = "В строке должны быть только латинские буквы в нижнем регистре! Неподходящие символы: " +
+                            Regex.Replace(Regex.Replace(inputLine, "[a-z]", ""), " ", " 'Пробел' ");
+                    }
+                    else
+                    {
+                        badRequestLine = "Строка не должна быть пустой!";
+                    }
+                    return BadRequest(badRequestLine);
+                }
+
+                if (jsonConfig.GetSection("Settings:BlackList").Get<string[]>().Any(str => str == inputLine))
+                {
+                    badRequestLine = $"'{inputLine}' находится в черном списке: ";
+                    foreach (string str in jsonConfig.GetSection("Settings:BlackList").Get<string[]>())
+                    {
+                        badRequestLine += str + ", ";
+                    }
+                    return BadRequest(badRequestLine);
+                }
+
+                char[] mainLine = inputLine.ToCharArray();
+
+                Array.Reverse(mainLine);
+                string resultLine;
+                if (mainLine.Length % 2 != 0)
+                {
+                    resultLine = new string(mainLine);
+                    Array.Reverse(mainLine);
+                    resultLine += new string(mainLine);
+
+                    resultData.ResultLine = resultLine;
+
                 }
                 else
                 {
-                    badRequestLine = "Строка не должна быть пустой!";
+                    var lastSegment = new ArraySegment<char>(mainLine, 0, mainLine.Length / 2);
+                    var firstSegment = new ArraySegment<char>(mainLine, mainLine.Length / 2, mainLine.Length / 2);
+                    resultLine = String.Join("", firstSegment) + (String.Join("", lastSegment));
+                    resultData.ResultLine = resultLine;
+                    Array.Reverse(mainLine);
                 }
-                return BadRequest(badRequestLine);
-            }
 
-            if (jsonConfig.GetSection("Settings:BlackList").Get<string[]>().Any(str => str == inputLine))
-            {
-                badRequestLine = $"'{inputLine}' находится в черном списке: ";
-                foreach (string str in jsonConfig.GetSection("Settings:BlackList").Get<string[]>())
+                resultData.CharsAmounts = new Dictionary<char, int>();
+                foreach (char letter in mainLine)
                 {
-                    badRequestLine += str + ", ";
+                    resultData.CharsAmounts[letter] = resultLine.Count(lt => lt == letter);
                 }
-                return BadRequest(badRequestLine);
-            }
 
-            char[] mainLine = inputLine.ToCharArray();
+                string maxLine = "";
 
-            Array.Reverse(mainLine);
-            string resultLine;
-            if (mainLine.Length % 2 != 0)
-            {
-                resultLine = new string(mainLine);
-                Array.Reverse(mainLine);
-                resultLine += new string(mainLine);
-
-                resultData.ResultLine = resultLine;
-
-            }
-            else
-            {
-                var lastSegment = new ArraySegment<char>(mainLine, 0, mainLine.Length / 2);
-                var firstSegment = new ArraySegment<char>(mainLine, mainLine.Length / 2, mainLine.Length / 2);
-                resultLine = String.Join("", firstSegment) + (String.Join("", lastSegment));
-                resultData.ResultLine = resultLine;
-                Array.Reverse(mainLine);
-            }
-
-            resultData.CharsAmounts = new Dictionary<char, int>();
-            foreach (char letter in mainLine)
-            {
-                resultData.CharsAmounts[letter] = resultLine.Count(lt => lt == letter);
-            }
-
-            string maxLine = "";
-
-            foreach (Match match in Regex.Matches(resultLine, "[aeiouy].*[aeiouy]"))
-            {
-                if (maxLine.Length < match.Value.Length)
+                foreach (Match match in Regex.Matches(resultLine, "[aeiouy].*[aeiouy]"))
                 {
-                    maxLine = match.Value;
+                    if (maxLine.Length < match.Value.Length)
+                    {
+                        maxLine = match.Value;
+                    }
                 }
-            }
 
-            resultData.LongSubline = maxLine;
+                resultData.LongSubline = maxLine;
 
-            if (sortOption != 1 && sortOption != 2)
-            {
-                return BadRequest("1 - quick sort; 2 - tree sort!");
-            }
-
-            char[] resultLineChars = resultLine.ToCharArray();
-
-            Quicksort quicksort = new Quicksort();
-            Treesort treesort = new Treesort();
-
-            if (sortOption == 1)
-            {
-                resultData.SortResultLine = new string(quicksort.QuicksortLogic(resultLineChars, 0, resultLineChars.Length - 1));
-            }
-            else if (sortOption == 1)
-            {
-                resultData.SortResultLine = new string(treesort.TreeSort(resultLineChars));
-            }
-
-            int delIndex;
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(jsonConfig.GetSection("RandomApi").Get<string>() + $"?min=0&max={resultLine.Length}&count=1");
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+                if (sortOption != 1 && sortOption != 2)
                 {
-                    delIndex = (streamReader.ReadToEnd()[1] - '0');
+                    return BadRequest("1 - quick sort; 2 - tree sort!");
                 }
+
+                char[] resultLineChars = resultLine.ToCharArray();
+
+                Quicksort quicksort = new Quicksort();
+                Treesort treesort = new Treesort();
+
+                if (sortOption == 1)
+                {
+                    resultData.SortResultLine = new string(quicksort.QuicksortLogic(resultLineChars, 0, resultLineChars.Length - 1));
+                }
+                else if (sortOption == 1)
+                {
+                    resultData.SortResultLine = new string(treesort.TreeSort(resultLineChars));
+                }
+
+                int delIndex;
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(jsonConfig.GetSection("RandomApi").Get<string>() + $"?min=0&max={resultLine.Length}&count=1");
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        delIndex = (streamReader.ReadToEnd()[1] - '0');
+                    }
+                }
+                catch (WebException)
+                {
+                    Random rnd = new Random();
+                    delIndex = rnd.Next(0, resultLine.Length);
+                }
+
+                resultData.ReduceLine = resultLine.Remove(delIndex, 1);
+
+                return Ok(resultData);
             }
-            catch (WebException)
+            finally 
             {
-                Random rnd = new Random();
-                delIndex = rnd.Next(0, resultLine.Length);
+                semaphore.Release();
             }
-
-            resultData.ReduceLine = resultLine.Remove(delIndex, 1);
-
-            return Ok(resultData);
         }
     }
 }
